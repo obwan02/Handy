8f0729e4f8a53f8d6058008e527f9fb1c87f3c2e
obwan02 <obwan02@hotmail.co.nz>
2023-03-09 14:24:53 +1300

    update: Created example Qt5 app

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+
+def main():
+    app = QApplication(sys.argv)
+    widget = QWidget()
+
+    textLabel = QLabel(widget)
+    textLabel.setText("Hello World!")
+    textLabel.move(110, 85)
+
+    widget.setGeometry(50, 50, 320, 200)
+    widget.setWindowTitle("PyQt5 Example")
+    widget.setWindowIcon(QIcon("assets/icon.png"))
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
